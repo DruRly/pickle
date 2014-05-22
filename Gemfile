72835098bcd90c147f77b9b6218adb1c8a294906
dev <cdx-dev@manheim.com>
2014-05-22 16:44:38 -0400

    Draft post '/failure' route

---
+++ Gemfile
@@ -0,0 +1,3 @@
+
+gem 'sinatra', '1.4.5'
+gem 'mongo', '1.10.1'
--- app.rb
+++ app.rb
@@ -1,1 +1,18 @@
+require 'rubygems'
 require 'sinatra'
+require 'mongo'
+require 'json/ext' # required for .to_json
+
+include Mongo
+
+configure do
+  conn = MongoClient.new("localhost", 27017)
+  set :mongo_connection, conn
+  set :mongo_db, conn.db('pickle')
+end
+
+post '/failure' do
+  content_type :json
+  new_id = settings.mongo_db['test'].insert params
+  document_by_id(new_id)
+end
