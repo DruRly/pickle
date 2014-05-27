82e4ac2bc9c9fb2f2852dd7ec39154bac3a7f36d
dev <cdx-dev@manheim.com>
2014-05-27 16:15:05 -0400

    Set db conditionally

--- app.rb
+++ app.rb
@@ -8,11 +8,13 @@ include Mongo
 configure do
   if ENV['MONGOLAB_URI']
     conn = MongoClient.from_uri(ENV['MONGOLAB_URI'])
+    set :mongo_connection, conn
+    set :mongo_db, conn.db
   else
     conn = MongoClient.new("localhost", 27017)
+    set :mongo_connection, conn
+    set :mongo_db, conn.db('pickle')
   end
-  set :mongo_connection, conn
-  set :mongo_db, conn.db('pickle')
 end
 
 get '/failures' do
